02399fe07a9e24221893025b9683b40114dd3fea
Richard Hillebrand (Mirasaki) <me@mirasaki.dev>
2026-05-04 19:08:36 +0200

    feat(http): add schemas for HTTP error response handling

---
+++ .changeset/gold-months-smoke.md
@@ -0,0 +1,5 @@
+---
+"@md-oss/http-client": patch
+---
+
+add http error response schemas
--- vendor/common/src/http/index.ts
+++ vendor/common/src/http/index.ts
@@ -2,5 +2,6 @@ export * from './errors';
 export * from './guards';
 export * from './headers';
 export * from './requests';
+export * from './schemas';
 export * from './status-codes';
 export * from './types';
