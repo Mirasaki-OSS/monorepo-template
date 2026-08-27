3a31e88e8fe04be009acdac0e41213a04170a19e
Richard Hillebrand (Mirasaki) <me@mirasaki.dev>
2026-08-27 16:23:25 +0200

    chore: pass biome ruleset

--- vendor/design-system/src/types/data-table.ts
+++ vendor/design-system/src/types/data-table.ts
@@ -17,12 +17,10 @@ import type { DataTableConfig } from '../config/data-table';
 import type { FilterItemSchema } from '../lib/parsers';
 
 declare module '@tanstack/react-table' {
-	// biome-ignore lint/correctness/noUnusedVariables: TData is used in the TableMeta interface
 	interface TableMeta<TFeatures extends TableFeatures, TData extends RowData> {
 		queryKeys?: QueryKeys;
 	}
 
-	// biome-ignore lint/correctness/noUnusedVariables: TData and TValue are used in the ColumnMeta interface
 	interface ColumnMeta<
 		TFeatures extends TableFeatures,
 		TData extends RowData,
